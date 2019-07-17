786673089509eef51e0641da76ea1e3f7b49e4b5
Josh Atchley <josh@vector-us.com>
2019-07-17 18:55:13 -0400

    New EnemiesAttach branch to get around issues with other branch having bad Unity checkins

--- Assets/Scripts/BirdBehaviour.cs
+++ Assets/Scripts/BirdBehaviour.cs
@@ -1,104 +1,178 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class BirdBehaviour : MonoBehaviour {
-	public static int score = 0;
-	public int scale = 1;
-	public static int maxScore = 999999999;
+public class BirdBehaviour : MonoBehaviour
+{
+    public static int score = 0;
+    public int scale = 1;
+    public static int maxScore = 999999999;
 
-	public float flapSpeed    = 175f;
-	public float forwardSpeed = 1f;
-	public float maxSpeed = 25f;
+    public float flapSpeed = 175f;
+    public float forwardSpeed = 1f;
+    public float maxSpeed = 25f;
 
-	private CircleCollider2D myCollider;
+    private CircleCollider2D myCollider;
 
-	public float gainSize = 1f;
-	public float gainMass = 0.2f;
-	public float gainSpeed = 0.2f;
+    public float gainSpeed = 0.2f;
 
-	public float loseSize = 0.2f;
-	public float loseMass = 0.04f;
-	public float loseSpeed = 1f;
+    public float loseSpeed = 1f;
 
-	private float counter = 0f; 
-	private float counterMass = 0f; 
-	private float counterSpeed = 0f;
+    private float counterSpeed = 0f;
 
-	bool didFlap = false;
-	Animator animator;
-	public bool dead = false;
-	public bool godMode = false;
+    bool didFlap = false;
+    Animator animator;
+    public bool dead = false;
+    public bool godMode = false;
 
-	public AudioClip whoosh;
-	public AudioClip splat;
-	public AudioClip impact;
-	AudioSource audioSource;
+    public AudioClip whoosh;
+    public AudioClip splat;
+    public AudioClip impact;
+    AudioSource audioSource;
 
-	void Start () {
-		audioSource = GetComponent<AudioSource>();
-		animator = transform.GetComponentInChildren<Animator>();
-	}
+    private List<EnemyBehaviour> attachedEnemies;
 
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) ) didFlap = true;
-		score = Mathf.RoundToInt(GameObject.FindGameObjectWithTag("Player").transform.position.x) * scale;   
-		if (score > maxScore) dead = true;
-		if (Time.timeScale == 0 && dead) Destroy(gameObject);
-	}
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = transform.GetComponentInChildren<Animator>();
+        attachedEnemies = new List<EnemyBehaviour>();
+    }
 
-	void FixedUpdate () {
-		if (dead) return;
-		GetComponent<Rigidbody2D>().AddForce( Vector2.right * forwardSpeed );
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) didFlap = true;
+        score = Mathf.RoundToInt(GameObject.FindGameObjectWithTag("Player").transform.position.x) * scale;
+        if (score > maxScore) dead = true;
+        if (Time.timeScale == 0 && dead) Destroy(gameObject);
+    }
 
-		if (didFlap) {
-			GetComponent<Rigidbody2D>().AddForce( Vector2.up * flapSpeed );
-			animator.SetTrigger("DoFlap");
-			audioSource.PlayOneShot(whoosh, 0.8F);
-			if (GetComponent<Rigidbody2D> ().mass >= 1.04f) {
-				counter = loseSize;
-				counterMass = loseMass;
-				GetComponent<Rigidbody2D> ().mass -= counterMass;
-				transform.localScale -= new Vector3 (counter, counter, counter);
-				counterMass = 0f;
-				counter = 0f;
-			}
-			if (GetComponent<Rigidbody2D> ().velocity.x <= maxSpeed) {
-				counterSpeed = gainSpeed;
-				forwardSpeed += counterSpeed;
-				counterSpeed = 0f;
-			}
-			didFlap = false;
-		}
+    void FixedUpdate()
+    {
+        if (dead) return;
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * forwardSpeed);
 
-		if (GetComponent<Rigidbody2D>().velocity.y > 0) {
-			float angle = Mathf.Lerp (0, 90, (-GetComponent<Rigidbody2D>().velocity.y / 3f) );
-			transform.rotation = Quaternion.Euler(0, 0, angle);
-		} else {
-			float angle = Mathf.Lerp (0, -90, (-GetComponent<Rigidbody2D>().velocity.y / 3f) );
-			transform.rotation = Quaternion.Euler(0, 0, angle);
-		}
-	}
+        if (didFlap)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * flapSpeed);
+            animator.SetTrigger("DoFlap");
+            audioSource.PlayOneShot(whoosh, 0.8F);
 
-	void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.gameObject.tag == "Enemy") {
-				audioSource.PlayOneShot(splat, 0.7F);
-				counter = gainSize;
-				counterMass = gainMass;
-				GetComponent<Rigidbody2D>().mass += counterMass;
-				transform.localScale += new Vector3 (counter, counter, counter);
-				counter = 0f;
-				counterMass = 0f;
-				if (GetComponent<Rigidbody2D> ().velocity.x >= 2) {
-					counterSpeed = loseSpeed;
-					forwardSpeed -= counterSpeed;
-					counterSpeed = 0f;
-				}
-		}
-		if (collision.gameObject.tag == "Limit") {
-				if (godMode) return;
-				audioSource.PlayOneShot(impact, 1F);
-				animator.SetTrigger("Death");
-				dead = true;
-		}
-	}
+            if (GetComponent<Rigidbody2D>().velocity.x <= maxSpeed)
+            {
+                counterSpeed = gainSpeed;
+                forwardSpeed += counterSpeed;
+                counterSpeed = 0f;
+            }
+
+            // TODO: Use this for weekly progress report. Talk about refactoring to need to store all the attached enemies to find the proper one to remove.
+            // TODO: Work on this
+            // Detach one enemy for every flap
+            if (attachedEnemies.Any())
+            {
+                // TODO: Also use this in weekly progress report. Attached enemy count was not accurate to the actual number of attached enemies 
+                // and was causing some issues once logic to attach on collision with attached enemy was implemented.
+                Debug.Log("Attached enemies count before flap removal: " + attachedEnemies.Count);
+
+                var mostRecentEnemy = attachedEnemies.OrderByDescending(x => x.index).FirstOrDefault();
+                var allAttachments = GetComponents<FixedJoint2D>();
+                FixedJoint2D enemyAttachment = null;
+                foreach (var attachment in allAttachments)
+                {
+                    var enemy = attachment.connectedBody.gameObject.GetComponent<EnemyBehaviour>();
+                    if (enemy != null && enemy.index == mostRecentEnemy.index)
+                    {
+                        enemyAttachment = attachment;
+                    }
+                }
+                // Remove the joint and decouple from the parent/child relationship
+                //Destroy(enemyAttachment);
+                //mostRecentEnemy.transform.parent = null;
+                //mostRecentEnemy.isConnected = false;
+                //attachedEnemies.Remove(mostRecentEnemy);
+
+                Debug.Log("Attached enemies count after flap removal: " + attachedEnemies.Count);
+            }
+
+            didFlap = false;
+        }
+
+        if (GetComponent<Rigidbody2D>().velocity.y > 0)
+        {
+            float angle = Mathf.Lerp(0, 90, (-GetComponent<Rigidbody2D>().velocity.y / 3f));
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else
+        {
+            float angle = Mathf.Lerp(0, -90, (-GetComponent<Rigidbody2D>().velocity.y / 3f));
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            var enemy = collision.gameObject.GetComponent<EnemyBehaviour>();
+            if (!enemy.connectedPreviously)
+            {
+                audioSource.PlayOneShot(splat, 0.7F);
+                if (GetComponent<Rigidbody2D>().velocity.x >= 2)
+                {
+                    counterSpeed = loseSpeed;
+                    forwardSpeed -= counterSpeed;
+                    counterSpeed = 0f;
+                }
+
+                // Enemies attach on collision
+                AttachToPlayer(enemy, collision);
+            }
+        }
+        if (collision.gameObject.tag == "Limit")
+        {
+            HandleDeath();
+        }
+    }
+
+    public void AttachToPlayer(EnemyBehaviour enemy, Collision2D collision)
+    {
+        Debug.Log("Attached enemies count before attachment add: " + attachedEnemies.Count);
+
+        // TODO: Make this work better when attached enemy collides with other enemy
+        collision.transform.parent = gameObject.transform;
+        var enemyAttachment = gameObject.AddComponent<FixedJoint2D>();
+        enemyAttachment.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
+        enemyAttachment.autoConfigureConnectedAnchor = true;
+
+        //Vector2 contactPoint = collision.contacts[0].point;
+        //enemyAttachment.anchor = gameObject.transform.InverseTransformPoint(contactPoint);
+
+        //Vector2 contactPoint = new Vector2(attachedEnemies.Count, attachedEnemies.Count);
+        //enemyAttachment.anchor = gameObject.transform.InverseTransformPoint(contactPoint);
+
+        //Debug.Log("Anchor contact point: " + contactPoint);
+
+        enemyAttachment.enableCollision = false;
+        enemyAttachment.dampingRatio = 1;
+        enemyAttachment.frequency = 0;
+        enemyAttachment.breakForce = Mathf.Infinity;
+        enemyAttachment.breakTorque = Mathf.Infinity;
+
+        // Ensure this enemy can't be collided with again
+        enemy.connectedPreviously = true;
+        enemy.isConnected = true;
+        enemy.index = attachedEnemies.Count;
+        attachedEnemies.Add(enemy);
+
+        Debug.Log("Attached enemies count after attachment add: " + attachedEnemies.Count);
+    }
+
+    public void HandleDeath()
+    {
+        if (godMode) return;
+        audioSource.PlayOneShot(impact, 1F);
+        animator.SetTrigger("Death");
+        dead = true;
+    }
 }
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,16 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class EnemyBehaviour : MonoBehaviour {
-	public float speed = 1f;
-	void start () {}
-	void Update () {
-		if (Time.timeScale == 0) Destroy(gameObject);
-	}
-	void FixedUpdate () {
-		GetComponent<Rigidbody2D>().AddForce( Vector2.left * speed );
-	}
-	void OnCollisionEnter2D(Collision2D collider) {
-		Destroy (gameObject);
-	}	
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float speed = 1f;
+    public bool isConnected = false;
+    public bool connectedPreviously = false;
+    public int index;
+
+    void start() { }
+    void Update()
+    {
+        if (Time.timeScale == 0) Destroy(gameObject);
+    }
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.left * speed);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If collision is with another enemy and the other enemy is attached to the player, attach this enemy to player as well
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+            var otherEnemy = collision.gameObject.GetComponent<EnemyBehaviour>();
+            if (otherEnemy.isConnected)
+            {
+                // TODO: Connect to player here using function in BirdBehaviour
+                Debug.Log("Enemy has attachment to player");
+                var player = GameObject.FindObjectOfType<BirdBehaviour>();
+                if (player != null)
+                {
+                    player.AttachToPlayer(gameObject.GetComponent<EnemyBehaviour>(), collision);
+                }
+            }
+        }
+        // TODO: Good point for progress report
+        // TODO: Make attached enemies go grey, be destroyed, or similar? How to handle attached enemies on death
+        if (collision.gameObject.tag == "Limit")
+        {
+            var player = GameObject.FindObjectOfType<BirdBehaviour>();
+            player.HandleDeath();
+        }
+    }
 }
