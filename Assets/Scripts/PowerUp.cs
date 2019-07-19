c79c092b6138dcb7853b52917652b248cf8df697
Jared Sulzdorf <j.sulzdorf@gmail.com>
2019-07-19 15:01:52 -0700

    adding new files

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public string powerUpName;
+    public GameObject soundfx;
+
+    public BirdBehaviour Bird;
+
+    public bool expiresOnTime;
+    private IEnumerator coroutine;
+
+    void Update () {
+		// Check for game being paused before we delete the enemies
+		if (Time.timeScale == 0 && !PauseScreen.GamePaused ) 
+        {
+            Destroy(gameObject);
+        }
+	}
+    protected virtual void OnTriggerEnter2D( Collider2D other )
+    {
+
+        // if the game object has been tagged as a player
+        if( other.gameObject.tag == "Player" )
+        {
+
+            Bird = other.GetComponent<BirdBehaviour>();
+
+            GetComponent<CircleCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+
+            Debug.Log("Hello");
+
+            if( expiresOnTime )
+            {
+                coroutine = CountDown( Bird );
+                StartCoroutine(coroutine);
+            }
+            else 
+            {
+                Destroy( gameObject.transform.root.gameObject );
+            }
+
+        } 
+        // Otherwise don't do anything
+        else 
+        {
+            return;
+        }
+    }
+
+    protected virtual void powerUpEffect( BirdBehaviour player )
+    {
+        Debug.Log( "Added" );
+    }
+
+    protected virtual void removePowerUpEffect( BirdBehaviour player )
+    {
+        Debug.Log( "Removed" );
+    }
+
+    protected virtual IEnumerator CountDown( BirdBehaviour player )
+    {
+
+        powerUpEffect( player );
+        yield return new WaitForSeconds(10);
+        Destroy( gameObject.transform.root.gameObject );
+        removePowerUpEffect( player );
+
+    }
+}
---
+++ Assets/Scripts/SpawnPowerUps.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPowerUps : MonoBehaviour {
+	public GameObject SpeedPowerUp;
+	public GameObject InvincibilityPowerUp;
+	public float min = 0.2f;
+    public float max = 1.48f;
+	public float spawnTime = 1f;
+	public float spawnDelay = 10f;
+
+	void Start() {  
+		//https://docs.unity3d.com/ScriptReference/MonoBehaviour.InvokeRepeating.html
+		InvokeRepeating("Spawn", spawnTime, spawnDelay);
+	}
+	void Spawn() {  
+		var spawnPoint = new Vector2(transform.position.x, Random.Range(max, min));
+		//https://docs.unity3d.com/ScriptReference/Random.Range.html
+		int rand = Random.Range( 0, 2 );
+		//https://docs.unity3d.com/ScriptReference/Object.Instantiate.html
+		switch (rand)
+		{
+			case 0:
+				Instantiate(SpeedPowerUp, spawnPoint, Quaternion.identity);
+				break;
+			case 1:
+				Instantiate(InvincibilityPowerUp, spawnPoint, Quaternion.identity);
+				break;
+			default:
+				break;
+		}
+	}
+    
+}
