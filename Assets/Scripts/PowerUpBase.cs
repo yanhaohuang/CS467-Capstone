7722cd7bea9a4208510c9752a8163b1a1b4f75f9
Josh Atchley <josh@vector-us.com>
2019-08-05 20:07:40 -0400

    Basic weapon shot behavior and sprite

--- Assets/Scripts/PowerUpBase.cs
+++ Assets/Scripts/PowerUpBase.cs
@@ -7,11 +7,11 @@ public class PowerUpBase : MonoBehaviour
 {
     // We don't use this currently, but it's just a string; needs to be set in Unity
     public string powerUpName;
-    
+
     // Needs to be added on the prefab in unity so we know what clip to play
     public AudioClip soundclip;
-	// Our audio mixer - needs to be added on the prefab in unity so we know what mixer to user
-	public AudioMixer audioMixer;
+    // Our audio mixer - needs to be added on the prefab in unity so we know what mixer to user
+    public AudioMixer audioMixer;
 
     // A reference to our player - we grab it on collision, so it's not necessary to worry about doing anything in Unity
     public PlayerBehaviour player;
@@ -22,23 +22,24 @@ public class PowerUpBase : MonoBehaviour
     public bool destoryable = false;
     private IEnumerator coroutine;
 
-    void Update () {
-		// Check for game being paused before we delete the enemies
-		if (Time.timeScale == 0 && !PauseScreen.GamePaused ) 
+    void Update()
+    {
+        // Check for game being paused before we delete the enemies
+        if (Time.timeScale == 0 && !PauseScreen.GamePaused)
         {
             Destroy(gameObject);
         }
-	}
+    }
 
     /*
         When a player hits a powerup, disable the visible/interactable components of the powerup
         and apply the powerup either immediately or over the course of time.
      */
-    protected virtual void OnTriggerEnter2D( Collider2D other )
+    protected virtual void OnTriggerEnter2D(Collider2D other)
     {
 
         // if the game object has been tagged as a player
-        if( other.gameObject.tag == "Player" )
+        if (other.gameObject.tag == "Player")
         {
 
             // Then get the player
@@ -49,22 +50,22 @@ public class PowerUpBase : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
 
             // Do whatever special effects are needed
-            doSpecialEffects( player );
+            doSpecialEffects(player);
 
             // And determine how to manage the powerup
-            if( expiresOnTime )
+            if (expiresOnTime)
             {
-                coroutine = CountDown( player );
+                coroutine = CountDown(player);
                 StartCoroutine(coroutine);
             }
-            else 
+            else
             {
-                powerUpEffect( player );
+                powerUpEffect(player);
             }
 
-        } 
+        }
         // Otherwise don't do anything
-        else 
+        else
         {
             return;
         }
@@ -73,19 +74,19 @@ public class PowerUpBase : MonoBehaviour
     /*
         Kind of a stub function - the specific powerup classes do most of this work this is just here in case we need it
      */
-    protected virtual void powerUpEffect( PlayerBehaviour player )
+    protected virtual void powerUpEffect(PlayerBehaviour player)
     {
     }
 
     /*
         Destroy the powerup and reset animation
      */
-    protected virtual void removePowerUpEffect( PlayerBehaviour player )
+    protected virtual void removePowerUpEffect(PlayerBehaviour player)
     {
         // Then we destroy the powerup
-        if( destoryable && !expiresOnTime )
+        if (destoryable && !expiresOnTime)
         {
-            Destroy( gameObject.transform.root.gameObject );
+            Destroy(gameObject.transform.root.gameObject);
         }
         // and in all cases, reset the animation back
         player.setAnimation("Normal");
@@ -94,15 +95,15 @@ public class PowerUpBase : MonoBehaviour
     /*
         Play an audio clip when hitting a powerup
      */
-    protected virtual void doSpecialEffects( PlayerBehaviour player )
+    protected virtual void doSpecialEffects(PlayerBehaviour player)
     {
-		// Getting the master mixer
-		AudioMixerGroup[] audioMixGroup = audioMixer.FindMatchingGroups("Master");
-		AudioSource soundfx = gameObject.GetComponent<AudioSource>();
-		// And make sure we play through the proper mixer
-		soundfx.outputAudioMixerGroup = audioMixGroup[0];
+        // Getting the master mixer
+        AudioMixerGroup[] audioMixGroup = audioMixer.FindMatchingGroups("Master");
+        AudioSource soundfx = gameObject.GetComponent<AudioSource>();
+        // And make sure we play through the proper mixer
+        soundfx.outputAudioMixerGroup = audioMixGroup[0];
         // Then play our single powerup clip
-       soundfx.PlayOneShot(soundclip, 1.0f);
+        soundfx.PlayOneShot(soundclip, 1.0f);
 
     }
 
@@ -110,13 +111,13 @@ public class PowerUpBase : MonoBehaviour
         If the powerup is one that should work over a period of time then we 
         call the effect, wait for 10 seconds, destroy the game object and remove the powerup effect if needed
      */
-    protected virtual IEnumerator CountDown( PlayerBehaviour player )
+    protected virtual IEnumerator CountDown(PlayerBehaviour player)
     {
 
-        powerUpEffect( player );
+        powerUpEffect(player);
         yield return new WaitForSeconds(7);
-        removePowerUpEffect( player );
-        Destroy( gameObject.transform.root.gameObject );
+        removePowerUpEffect(player);
+        Destroy(gameObject.transform.root.gameObject);
 
     }
 }
---
+++ Assets/Scripts/WeaponShotBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShotBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        if (Time.timeScale == 0 && !PauseScreen.GamePaused) Destroy(gameObject);
+        GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+    }
+
+    void Update()
+    {
+        // Destroy the shot if it gets too far away
+        Transform playerTransform = FindObjectOfType<PlayerBehaviour>().transform;
+        float distance = transform.position.x - playerTransform.position.x;
+        Debug.Log("Distance: " + distance);
+        if (distance > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            // Destroy enemy on contact
+            Destroy(collision.gameObject);
+            // Destroy weapon shot object
+            Destroy(gameObject);
+        }
+    }
+}
